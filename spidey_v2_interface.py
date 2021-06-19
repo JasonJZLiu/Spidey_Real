e405b15161950ee3c79875aa9d78d6aba9ebf934
Jason <jason_liu1116@hotmail.com>
2021-06-19 00:55:32 +0000

    default state updated

--- spidey_v2_interface.py
+++ spidey_v2_interface.py
@@ -24,10 +24,10 @@ class Spidey_V2:
         self._state = self.default_state
         self._previous_state = self.default_state
 
-        self._default_servo_positions = [100, 75, 85,
-                                         100, 80, 105,
-                                         100, 75, 85,
-                                         90, 80, 100]
+        self._default_servo_positions = [90, 80, 90,
+                                         90, 80, 90,
+                                         90, 80, 90,
+                                         90, 80, 90]
                                         
         
         self._joint_to_channel_mapping = [6,  7,  8,
