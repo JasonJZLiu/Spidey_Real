99b562d086dbdd529d1c6b38463d2f430fd77358
Jingzhou Liu <jason_liu1116@hotmail.com>
2021-07-16 22:54:17 -0400

    added turning left

--- walking.py
+++ walking.py
@@ -21,14 +21,26 @@ def forward(spidey_interface):
         spidey_interface.apply_command(cmds[i])
         #time.sleep(0.2)
 
+def turning_left(spidey_interface):
+    infile = open("joint_cmds/turn_left_joint_cmd",'rb')
+    cmds = pickle.load(infile)
+    infile.close()
+
+    for i in range(len(cmds)):
+        spidey_interface.apply_command(cmds[i])
+        #time.sleep(0.2)
+
 
 
 if __name__ == "__main__":
     spidey_interface = Spidey()
     set_init_pose(spidey_interface)
     input("wait")
-    while True:
-        forward(spidey_interface)
+    
+    turning_left(spidey_interface)
+
+    # while True:
+    #     forward(spidey_interface)
 
 
 
